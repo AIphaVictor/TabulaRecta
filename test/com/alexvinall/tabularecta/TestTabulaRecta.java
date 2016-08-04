10802b900081f7670f846ab0ac2d15390512f7bf
Alex Vinall <AIphaVictor@users.noreply.github.com>
2016-08-04 11:39:21 +0100

    Added maven for dependency management.

---
+++ test/com/alexvinall/tabularecta/TestTabulaRecta.java
@@ -0,0 +1,15 @@
+package com.alexvinall.tabularecta;
+
+import org.junit.Test;
+
+/**
+ * Test class for {@link TabulaRecta}.
+ *
+ */
+public class TestTabulaRecta {
+
+  @Test
+  public void testTabulaRectaOutput() {
+
+  }
+}
